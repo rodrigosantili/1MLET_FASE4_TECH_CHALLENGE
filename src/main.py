f5cc5926b8f96bc73dcdfc16b0607176d9316df0
brunomcr <bruno.mcreal@gmailcom>
2024-11-10 09:53:51 -0300

    Add utility functions for device configuration and sequence generation

--- src/main.py
+++ src/main.py
@@ -3,8 +3,10 @@ from dotenv import load_dotenv
 
 from data import fetch_data, preprocess_data
 
+from utils.sequence_utils import create_sequences
+from utils.tensor_utils import prepare_tensors
+from utils.device_utils import get_device
 from mlflow_setup import init_mlflow, log_params
-
 
 load_dotenv()
 
@@ -14,12 +16,21 @@ def main():
 
     params = {
         "YFINANCE_TICKER": os.getenv('YFINANCE_TICKER'),
-        "YFINANCE_PERIOD": os.getenv('YFINANCE_PERIOD')
+        "YFINANCE_PERIOD": os.getenv('YFINANCE_PERIOD'),
+        "FRAMEWORK": "pytorch",  # Can be "pytorch" or "keras"
+        "SEQ_LENGTH": 60,
     }
     log_params(params)
 
+    device = get_device(params["FRAMEWORK"])
+    print(f"device {device}")
+
     data = fetch_data(ticker=params["YFINANCE_TICKER"],period=params["YFINANCE_PERIOD"])
     scaled_data, scaler = preprocess_data(data)
+
+    sequences, targets = create_sequences(scaled_data, params["SEQ_LENGTH"])
+    print(f"sequences {sequences}")
+    print(f"targets {targets}")
 
 
 if __name__ == '__main__':
---
+++ src/utils/device_utils.py
@@ -0,0 +1,25 @@
+import torch
+import tensorflow as tf
+
+
+def get_device(framework="pytorch"):
+    """
+    Returns the appropriate device for PyTorch or Keras/TensorFlow.
+
+    Parameters:
+        framework (str): The framework being used, either "pytorch" or "keras".
+
+    Returns:
+        device (torch.device or str): The device to be used, either "cuda" or "cpu" for PyTorch,
+                                      or "/GPU:0" or "/CPU:0" for Keras/TensorFlow.
+    """
+    if framework == "pytorch":
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(f"Using device for PyTorch: {device}")
+        return device
+    elif framework == "keras":
+        device = "/GPU:0" if tf.config.list_physical_devices('GPU') else "/CPU:0"
+        print(f"Using device for Keras: {device}")
+        return device
+    else:
+        raise ValueError("Unsupported framework. Choose 'pytorch' or 'keras'.")
